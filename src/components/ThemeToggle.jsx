8de04dadeddab46c0d0b69c9148cb81be57aafda
Kamal Kant <kamalkant164812@gmail.com>
2026-01-03 18:23:42 +0530

    togglechanges2

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,7 +33,6 @@ export const ThemeToggle = () => {
       onClick={toggleTheme}
       className={cn(
         "fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
       )}
     >
       {isDarkMode ? (
