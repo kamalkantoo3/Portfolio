28201c008e012c827ee8184dd7f825113578b392
Kamal Kant <kamalkant164812@gmail.com>
2026-01-06 11:34:31 +0530

    resumchange

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1lUzPHFtLJXFYMYEMzosM_P3IqsZbL5Lh/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1YbbA-rcjvwd1W-qltg5A79MM8Aodne3l/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
